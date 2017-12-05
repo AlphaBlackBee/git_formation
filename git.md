c594a206e988315b4075960eec3f6db8653ba6d7
michael barbarin <michael.barbarin@uavia.eu>
2017-12-05 17:41:32 +0100

    mv git.md and delete PITCHME and add assets and scripts

--- git.md
+++ git.md
@@ -1,4 +1,4 @@
-# GIT FORMATION
+# GIT
 
 ## Install
 
---
+++ scripts/gitclone.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git clone git@github.com:BarbareBzhdev/TestGit.git NewLocalGit
+cd NewLocalGit
+ls -la .
+git remote show origin
+exit
